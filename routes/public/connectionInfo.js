da43112e24cc68fdcfcd957c2fd3cc1841b82955
rodjeralej <rog4shell@gmail.com>
2020-02-26 11:51:40 -0500

    last connectioninfo endpoint

--- routes/public/connectionInfo.js
+++ routes/public/connectionInfo.js
@@ -6,14 +6,25 @@ module.exports = {
     path: '/connectionInfo',
     handler: async (ctx) => {
         
-        const {userCount, connectionCount, bytes, seconds} = JSON.parse(localStorage.getItem('usersInfo'));
-        const usersInfo = {
-            userCount,
-            connectionCount,
-            bytes: parseInt(bytes),
-            seconds: parseInt(seconds),             
+        const userInfo = JSON.parse(localStorage.getItem('usersInfo'));
+        let nationalInfo = JSON.parse(localStorage.getItem('usersNationalInfo'));
+        const internetInfo = {
+            userCount: userInfo.userCount - nationalInfo.userCount,
+            connectionCount: userInfo.connectionCount - nationalInfo.connectionCount,
+            bytes: parseInt(userInfo.bytes) * 1024  - parseInt(nationalInfo.bytes),
+            seconds: parseInt(userInfo.seconds - nationalInfo.seconds),
+
         }
-        ctx.body = usersInfo;
+        const {userCount, connectionCount, bytes, seconds} = nationalInfo;
+        nationalInfo = Object.assign({}, {userCount, connectionCount, bytes: parseInt(bytes),seconds: parseInt(seconds)});
+
+
+        const generalInfo = {
+            nationalInfo,
+            internetInfo,
+
+        }
+        ctx.body = generalInfo;
         ctx.status = 200;
     },
 }
